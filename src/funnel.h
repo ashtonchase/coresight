ff1699164ce1036c454b97297205dee5cdc3a12d
Ashton Johnson <acj0003@gmail.com>
2019-02-20 21:20:46 -0600

    updated funnel to compliant code.

--- src/funnel.h
+++ src/funnel.h
@@ -9,38 +9,58 @@
 #define SRC_FUNNEL_H_
 
 #define FUNNEL_BASE_ADDR 0xF8804000
-
-Control 0x00000000 12 rw 0x00000300 CSTF Control Register
-PriControl 0x00000004 24 rw 0x00FAC688 CSTF Priority Control Register
-ITATBDATA0 0x00000EEC 5 rw 0x00000000 Integration Test ATB Data 0
-Register
-ITATBCTR2 0x00000EF0 2 rw 0x00000000 Integration Test ATB Control 2
-Register
-ITATBCTR1 0x00000EF4 7 rw 0x00000000 Integration Test ATB Control 1
-Register
-ITATBCTR0 0x00000EF8 10 mixed 0x00000000 Integration Test ATB Control 0
-Register
-IMCR 0x00000F00 1 rw 0x00000000 Integration Mode Control
-Register
-CTSR 0x00000FA0 4 rw 0x0000000F Claim Tag Set Register
-CTCR 0x00000FA4 4 rw 0x00000000 Claim Tag Clear Register
-LAR 0x00000FB0 32 wo 0x00000000 Lock Access Register
-LSR 0x00000FB4 3 ro 0x00000003 Lock Status Register
-ASR 0x00000FB8 8 ro 0x00000000 Authentication Status Register
-DEVID 0x00000FC8 8 ro 0x00000028 Device ID
-DTIR 0x00000FCC 8 ro 0x00000012 Device Type Identifier Register
-PERIPHID4 0x00000FD0 8 ro 0x00000004 Peripheral ID4
-PERIPHID5 0x00000FD4 8 ro 0x00000000 Peripheral ID5
-PERIPHID6 0x00000FD8 8 ro 0x00000000 Peripheral ID6
-PERIPHID7 0x00000FDC 8 ro 0x00000000 Peripheral ID7
-PERIPHID0 0x00000FE0 8 ro 0x00000008 Peripheral ID0
-PERIPHID1 0x00000FE4 8 ro 0x000000B9 Peripheral ID1
-PERIPHID2 0x00000FE8 8 ro 0x0000001B Peripheral ID2
-PERIPHID3 0x00000FEC 8 ro 0x00000000 Peripheral ID3
-COMPID0 0x00000FF0 8 ro 0x0000000D Component ID0
-COMPID1 0x00000FF4 8 ro 0x00000090 Component ID1
-COMPID2 0x00000FF8 8 ro 0x00000005 Component ID2
-COMPID3 0x00000FFC 8 ro 0x000000B1 Component ID3
+//* CSTF Control Register */
+#define CONTROL 0x00000000
+//* CSTF Priority Control Register */
+#define PRICONTROL 0x00000004
+//* Integration Test ATB Data 0 Register */
+#define ITATBDATA0 0x00000EEC
+//* Integration Test ATB Control 2 Register */
+#define ITATBCTR2 0x00000EF0
+//* Integration Test */
+#define ITATBCTR1 0x00000EF4
+//* Integration Test ATB Control 0 Register */
+#define ITATBCTR0 0x00000
+//* Integration Mode Control Register */EF8  
+#define IMCR 0x00000F00
+//* Claim Tag Set Register */
+#define CTSR 0x00000FA0
+//* Claim Tag Clear Register */
+#define CTCR 0x00000FA4
+//* Lock Access Register */
+#define LAR 0x00000FB0
+//* Lock Status Register */
+#define LSR 0x00000FB4
+//* Authentication Status Register */
+#define ASR 0x00000FB8
+//*  Device ID */
+#define DEVID 0x00000FC8
+//* Device Type Identifier Register */
+#define DTIR 0x00000FCC
+//* Peripheral ID4 */
+#define PERIPHID4 0x00000FD0
+//* Peripheral ID5 */
+#define PERIPHID5 0x00000FD4
+//* Peripheral ID6 */
+#define PERIPHID6 0x00000FD8
+//* Peripheral ID7 */
+#define PERIPHID7 0x00000FDC
+//* Peripheral ID0 */
+#define PERIPHID0 0x00000FE0
+//* Peripheral ID1 */
+#define PERIPHID1 0x00000FE4
+//* Peripheral ID2 */
+#define PERIPHID2 0x00000FE8
+//* Peripheral ID3 */
+#define PERIPHID3 0x00000FEC
+//* Component ID0 */
+#define COMPID0 0x00000FF0
+//* Component ID1 */
+#define COMPID1 0x00000FF4
+//* Component ID2 */
+#define COMPID2 0x00000FF8
+//* Component ID3 */
+#define COMPID3 0x00000FFC  
 
 
 #endif /* SRC_FUNNEL_H_ */
